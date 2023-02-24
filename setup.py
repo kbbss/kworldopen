b101164002eb54b26e5ad8fbefea89abdfe68a8f
kebiat <kebiat@naver.com>
2023-02-24 17:46:52 +0900

    Sd2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
    author='kebiat',
    url="https://github.com/kbbss/kworldopen.git",
    author_email='kebiat@naver.com',
-   packages=find_packages(where='osul'), # would be the same as name
+   packages=find_packages(where=''),
    license="kebiat",
    zip_safe=False,
    install_requires = ["pymongo==3.12.0"]
