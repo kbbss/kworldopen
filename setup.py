006856bd17da64a704c23dbb81697d80d92769b5
kebiat <kebiat@naver.com>
2023-02-24 16:49:11 +0900

    Sd

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
    version='0,9,1',
    description='Something different',
    author='kebiat',
+   url="https://github.com/kbbss/kworldopen.git",
    author_email='kebiat@naver.com',
    packages=['klsword','osul'],  # would be the same as name
+   zip_safe=False,
+   install_requires = ["pymongo==3.12.0"]
 
 )
