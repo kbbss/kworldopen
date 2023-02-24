c228130966d7a23796ab33d623c6c5a096d8d58f
kebiat <kebiat@naver.com>
2023-02-24 17:00:25 +0900

    Sd

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
    name='klswordopen',
-   version='0,9,1',
-   description='Something different',
+   version='0.0.1',
+   description='klsworld open!',
    author='kebiat',
    url="https://github.com/kbbss/kworldopen.git",
    author_email='kebiat@naver.com',
    packages=['osul'],  # would be the same as name
+   license="kebiat",
    zip_safe=False,
    install_requires = ["pymongo==3.12.0"]
 
