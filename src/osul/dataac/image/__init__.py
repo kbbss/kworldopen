489e73705aeacbe8b556cf07b79895250c7ea3b3
kebiat <kebiat@naver.com>
2023-02-25 13:46:47 +0900

    Sd2

--- src/osul/dataac/image/__init__.py
+++ src/osul/dataac/image/__init__.py
@@ -1,6 +1,9 @@
 from ...dataac import fromID
 from urllib.request import urlopen
 from PIL import Image
+import numpy as np
+import urllib.request
+import cv2
 
 host = "http://miyo2020.cafe24.com"
 
@@ -11,4 +14,13 @@ def imageUrl(id):
     # ${acfile.imageUrl}${image.path}/${image.id}.${image.exa}`
     if image is not None:
         return f"{host}/acfile/data/{image['path']}/{str(image['_id'])}.{image['exa']}"
+    return None
+
+
+def imageCv2Array(id):
+    url = imageUrl(id)
+    if url is not None:
+        resp = urllib.request.urlopen(url)
+        image = np.asarray(bytearray(resp.read()), dtype='uint8')
+        return cv2.imdecode(image, cv2.IMREAD_COLOR)
     return None
