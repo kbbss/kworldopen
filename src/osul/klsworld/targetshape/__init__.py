541f60879ae4b82bd894764f32299fbb019698df
kebiat <kebiat@naver.com>
2023-02-25 19:48:36 +0900

    Sd2

--- src/osul/klsworld/targetshape/__init__.py
+++ src/osul/klsworld/targetshape/__init__.py
@@ -1,5 +1,7 @@
 from ...monogodb import db
 from bson.objectid import ObjectId
+from ...stablediffusion import makeImage as sd_makeImage
+from ...dataac.image import upload
 
 collection = db["klsworld_targetshape"]
 
@@ -7,3 +9,20 @@ collection = db["klsworld_targetshape"]
 def find(model_name, target):
     return collection.find_one(
         {"model_name": model_name, "target._id": ObjectId(target["id"]), "target.name": target["name"]})
+
+
+def createImage(model_name, target):
+    ts = find(model_name, target)
+    if ts:
+        img = sd_makeImage(ts["prompt"])
+        if img:
+            filename = "targetshape_create_image.jpg"
+            img.save(filename)
+
+            clist= upload(filename, "/sdtest")
+            print("clist",clist)
+            return img
+
+    else:
+        print(f"targetshapenone model_name={model_name} ,target={target}")
+        return None
