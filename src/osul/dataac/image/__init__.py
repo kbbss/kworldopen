01c7ee09399d9e354428734a11c23c6c85ba2773
kebiat <kebiat@naver.com>
2023-02-25 13:34:54 +0900

    Sd2

--- src/osul/dataac/image/__init__.py
+++ src/osul/dataac/image/__init__.py
@@ -1,17 +1,14 @@
-
 from ...dataac import fromID
 from urllib.request import urlopen
 from PIL import Image
 
 host = "http://miyo2020.cafe24.com"
 
-def imageView(id):
+
+def imageUrl(id):
     image = fromID(id)
-    print("image",image)
-    #${acfile.imageUrl}${image.path}/${image.id}.${image.exa}`
+    print("image", image)
+    # ${acfile.imageUrl}${image.path}/${image.id}.${image.exa}`
     if image is not None:
-        url = f"{host}/acfile/data/{image['path']}/{str(image['_id'])}.{image['exa']}"
-        print("url!",url)
-        img = Image.open(urlopen(url))
-        print("img",img)
-        img.show()
+        return f"{host}/acfile/data/{image['path']}/{str(image['_id'])}.{image['exa']}"
+    return None
